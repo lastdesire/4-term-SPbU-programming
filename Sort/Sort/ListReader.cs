6938781b848d4f51c7d40b459a8071588814ed17
lastdesire <deadinsidezz123450@mail.ru>
2023-03-29 14:04:59 +0300

    Uploaded list reader

---
+++ Sort/Sort/ListReader.cs
@@ -0,0 +1,73 @@
+﻿namespace Sort;
+
+public static class ListReader
+{
+    public static bool WithError { get; private set;  }
+
+    private static List<string>? GetStringListFromFile(string path)
+    {
+        WithError = true;
+        
+        var str = "";
+        try
+        {
+            using (var streamReader = new StreamReader(path))
+            {
+                str = streamReader.ReadToEnd();
+                WithError = false;
+            }
+        }
+        catch
+        {
+            WithError = true;
+        }
+
+        return WithError ? null : str.Split(' ').ToList();
+    }
+
+    public static List<int> GetIntListFromFile(string path)
+    {
+        var strList = GetStringListFromFile(path);
+
+        var intList = new List<int>();
+        
+        if (WithError)
+        {
+            return intList;
+        }
+
+        foreach (var item in strList)
+        {
+            if (!int.TryParse(item, out var j))
+            {
+                WithError = true;
+                return intList;
+            }
+            intList.Add(j);
+        }
+
+        return intList;
+    }
+
+    private static string GetStringFromIntList(List<int> list)
+    {
+        return string.Join(' ', list);
+    }
+
+    public static void WriteIntListInFile(List<int> list, string path)
+    {
+        WithError = true;
+        try
+        {
+            using (var streamWriter = new StreamWriter(path))
+            {
+                streamWriter.WriteLine(GetStringFromIntList(list));
+                WithError = false;
+            }
+        }
+        catch
+        {
+            WithError = true;
+        }
+    }
+}
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using MPI;
+
+namespace Sort;
+
+public static class Sort
+{
+    public static int Main(string[] args)
+    {
+        using (var env = new MPI.Environment(ref args))
+        {
+            var comm = Communicator.world;
+            var rank = comm.Rank;
+            
+            if (0 == rank && 2 != args.Length)
+            {
+                Console.WriteLine("Expected paths to input and output files as arguments.");
+                return -1;
+            }
+            
+            var size = comm.Size;
+
+            if (0 == rank)
+            {
+                var list = ListReader.GetIntListFromFile(args[0]);
+
+                if (ListReader.WithError)
+                {
+                    Console.WriteLine($"Error trying read data from file with path {args[0]}");
+                    return -1;
+                }
+                else
+                {
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+         
+            return 0;
+        }
+    }
+}
